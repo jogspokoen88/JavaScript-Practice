b8ac7f7e21fcdc810c0bc5abd59fab61f88689ed
Sergei <jogspokoen88@mail.ru>
2020-05-14 16:51:42 +0500

    Получение элементов со страницы

--- task_1/script.js
+++ task_1/script.js
@@ -74,10 +74,15 @@ if (appData.moneyPerDay < 100) {
 
     chooseIncome: function() {
         let items = prompt("Что принесеет дополнительный доход(Перечислите через запятую)", "")
+
+        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
+            console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        } else {
         appData.income = items.split(', ')
         appData.income.push(prompt("Может что-то еще?"))
         appData.income.sort()
-
+        }
+            
         appData.income.forEach(function(itemmassive, i) {
             alert("Способы доп. заработка: " + (i+1) + " - " + itemmassive)
         })
