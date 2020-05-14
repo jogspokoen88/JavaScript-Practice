b6df1d729f0a637a3cc75970b3ac1b5cbe7eb5b8
Sergei <jogspokoen88@mail.ru>
2020-05-14 18:10:20 +0500

    Получение элементов приложения

---
+++ task-4/js/main.js
@@ -0,0 +1,27 @@
+let startButton = document.getElementById('start')
+let budget = document.getElementsByClassName('budget-value')
+let daybudget = document.getElementsByClassName('daybudget-value')
+let level = document.getElementsByClassName('level-value')
+let expenses = document.getElementsByClassName('expenses-value')
+let optionalexpenses = document.getElementsByClassName('optionalexpenses-value')
+let income = document.getElementsByClassName('income-value')
+let monthsavings = document.getElementsByClassName('monthsavings-value')
+let yearsavings = document.getElementsByClassName('yearsavings-value')
+
+let year = document.getElementsByClassName('year-value')
+let month = document.getElementsByClassName('month-value')
+let day = document.getElementsByClassName('day-value')
+
+let expensesItem = document.querySelectorAll('.expenses-item')
+
+let approveExpenses = document.getElementsByTagName('button')
+let approveBudget = document.getElementsByTagName('button')
+let calculate = document.getElementsByTagName('button')
+
+let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item')
+
+let countBudget = document.getElementsByClassName('count-budget')
+let chooseIncomeLabel = document.getElementsByClassName('choose-income-label')
+let checkbox = document.getElementById('savings')
+let sum = document.getElementsByTagName('label')
+let percent = document.getElementsByTagName('label')
